9417562d1b51e24b61ae770989eb35e08ec95045
asmaaabdeen <asmaa.abdeen_2000@hotmail.com>
2021-06-11 14:22:45 +0200

    create and edit post

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,70 +1,182 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                     Create Post 
-                </div>
-                
-            </div>
-        </div>
-    </div>
-    @if ($errors->any())
-    <div class="alert alert-danger">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Contact - Moderna Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="/assets/img/favicon.png" rel="icon">
+  <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="/assets/css/style.css" rel="stylesheet">
+
+
+  <!-- =======================================================
+  * Template Name: Moderna - v4.3.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>Moderna</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
         <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
+          <li><a class="" href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="team.html">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class="active" href="contact.html">Contact Us</a></li>
         </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
     </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Contact Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Create Post</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Create Post</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section class="contact" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+      <div class="container">
+
+      @if ($errors->any())
+  <div class="alert alert-danger">
+      <ul>
+          @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+          @endforeach
+      </ul>
+  </div>
 @endif
-
-    <div class="w-4/5 m-auto pt-20">
-        <form 
-            action="/blog"
-            method="POST"
-            enctype="multipart/form-data"
-            >
-            @csrf
-
-            <label class="bg-transperant block border-b-2 w-full h-20 text-5xl outline-none" for="title">Title</label>
-            <input 
-                type="text"
-                name="title"
-                placeholder="Title..."
-                class="bg-transperant block border-b-2 w-full h-20 text-6xl outline-none" >
+          <div class="col-lg-6">
+            <form action="/blog"
+          method="POST"
+          enctype="multipart/form-data">
+          @csrf
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="title" class="form-control" id="title" placeholder="Post Title" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
                 <label  class="bg-transperant block border-b-2 w-full h-20 text-5xl outline-none" for="file">Upload Post Image</label>
-                <input type="file" name="file" required>
-                <label class="bg-transperant block border-b-2 w-full h-20 text-5xl outline-none" for="channel">Channel</label>
-                <select name="channel" required>
-                    <option >--Select Channel--</option>
+              <input type="file" name="file" required>
+              
+                </div>
+              </div>
+              <div class="form-group mt-3">
+              <label class="bg-transperant block border-b-2 w-full h-20 text-5xl outline-none" for="channel">Channel</label>
+              <select name="channel" required>
+                  <option >--Select Channel--</option>
 
-                    @foreach ($channels as $channel)
-                    <option value="{{ $channel->id }}" >{{ $channel->name }}</option>
+                  @foreach ($channels as $channel)
+                  <option value="{{ $channel->id }}" >{{ $channel->name }}</option>
 
-                    @endforeach
-                </select>
+                  @endforeach
+              </select>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="description" rows="5" placeholder="Post Body" required></textarea>
+              </div>
+              <div class="my-3">
+               
+                <div class="error-message"></div>
+                
+              </div>
+              <div class="text-center"><button type="submit">Submit Post</button></div>
+            </form>
+          </div>
 
-                <textarea 
-                    name="description"
-                    placeholder="Description..."
-                    class="py-20 bg-transperant block border-b-2 w-full h-60 text-xl outline-none"
-                    ></textarea>
-                    <br>
-                    <button 
-                    type="submit"
-                    class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                    Submit Post
-                    </button>
+        </div>
 
+      </div>
+    </section><!-- End Contact Section -->
 
+    <!-- ======= Map Section ======= -->
+   
+  </main><!-- End #main -->
 
-        </form>
-    </div>
-</x-app-layout>
+  <!-- ======= Footer ======= -->
+ 
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="/assets/vendor/aos/aos.js"></script>
+  <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="/assets/vendor/php-email-form/validate.js"></script>
+  <script src="/assets/vendor/purecounter/purecounter.js"></script>
+  <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="/assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="/assets/js/main.js"></script>
+
+</body>
+
+</html>
