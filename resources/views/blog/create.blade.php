f14c38e965a234aaca0f31f38612e7119cb7bc6f
asmaaabdeen <asmaa.abdeen_2000@hotmail.com>
2021-07-04 01:46:23 +0200

    added user photo in nav bar

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -61,26 +61,8 @@
                 <a href="/blog" class=" mr-6  p-3  text-base font-bold uppercase  ">
                     {{ __('Blog') }}
                 </a>
-
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-         
-        </ul>
+                <li class="nav-img"><img style="width: 20px; " class="img-profile rounded-circle" src="{{ asset('storage/Users/' . Auth::user()->userImage()) }}"> {{ Auth::user()->name }}</li>  
+          
         <i class="bi bi-list mobile-nav-toggle"></i>
         
         </div>
